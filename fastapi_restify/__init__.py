be2fda05b9e04afc745fdcf3d31f2abe75ecf20e
Steve Ryan <syntithenai@gmail.com>
2021-08-21 22:03:50 +1000

    bump version

--- fastapi_restify/__init__.py
+++ fastapi_restify/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
 # Version of the package
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="fastapi_restify",
-    version="0.0.1",
+    version="0.0.4",
     description="Rest API configured with pydantic, backed by file database or mongo",
     long_description=README,
     long_description_content_type="text/markdown",
